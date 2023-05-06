307a81ebd3ec544c300fff0c48cf2e744c1e2bdf
IT21241550 <99632712+IT21241550@users.noreply.github.com>
2023-05-07 00:40:17 +0530

    Product_done

--- app/src/main/java/com/example/slcraft/models/Calculate.kt
+++ app/src/main/java/com/example/slcraft/models/Calculate.kt
@@ -1,5 +1,5 @@
 package com.example.slcraft.models
 
-class Calculate (private val quantity:Double, private val price:Double){
+class Calculate (private val quantity:Int, private val price:Double){
     fun multiply() = quantity*price
 }
--- app/src/main/java/com/example/slcraft/models/ItemModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.slcraft.models
-
-data class ItemModel (
-    var itemId: String? = null,
-    var itemNumber: String? = null,
-    var itemQty: String? = null,
-    var itemUnitPrice: String? = null,
-)
