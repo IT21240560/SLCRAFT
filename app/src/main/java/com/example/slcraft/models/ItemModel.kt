34907489cd9eb508ff82414c81d4fff64403bbec
Chamod <it21240560@my.sliit.lk>
2023-05-06 16:58:00 +0530

    initial commit

---
+++ app/src/main/java/com/example/slcraft/models/ItemModel.kt
@@ -0,0 +1,8 @@
+package com.example.slcraft.models
+
+data class ItemModel (
+    var itemId: String? = null,
+    var itemNumber: String? = null,
+    var itemQty: String? = null,
+    var itemUnitPrice: String? = null,
+)
